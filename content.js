40c7ceddf6f186b9fa7d38755b95f88b768825af
Michael Churilov <michael.churilov@gmail.com>
2020-11-16 17:41:37 -0500

    Add escape key as clear sequence

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
-document.addEventListener("keydown", (event) => {
-  if (event.code === "KeyC" && event.ctrlKey) {
-    const input = document.getElementById("user-response");
-    input.value = "";
-  }
-});
+const isClearCommand = (event) => {
+    if (event.key === 'c' && event.ctrlKey) return true
+    if (event.key === 'Escape') return true
+}
+
+document.addEventListener('keydown', (event) => {
+    if (isClearCommand(event)) {
+        const input = document.getElementById('user-response')
+        input.value = ''
+    }
+})
